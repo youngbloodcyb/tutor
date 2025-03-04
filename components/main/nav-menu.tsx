cd82da7d93e700f35fbbc497903607f56518dfd1
Cameron Youngblood <cameron.youngblood@gmail.com>
2025-03-03 21:12:45 -0800

    feat(ui): add navigation

---
+++ components/main/nav-menu.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import * as React from "react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
+const navItems = [
+  {
+    title: "Dashboard",
+    href: "/dashboard",
+  },
+  {
+    title: "Courses",
+    href: "/courses",
+  },
+  {
+    title: "Chats",
+    href: "/chats",
+  },
+  {
+    title: "Settings",
+    href: "/settings",
+  },
+];
+
+export function NavMenu() {
+  return (
+    <NavigationMenu className="z-[5] w-full flex justify-between px-2 py-3">
+      <div className="flex items-center gap-2">
+        <Link href="/">
+          <span className="text-xl font-heading">TUTOR</span>
+        </Link>
+        <NavigationMenuList className="w-full">
+          {navItems.map((item, index) => (
+            <NavigationMenuItem key={index}>
+              <Link href={item.href} legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  <span className="m750:max-w-[80px] m750:text-xs">
+                    {item.title}
+                  </span>
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </div>
+      <div className="flex items-center gap-2">
+        <Button>Login</Button>
+        <Button variant="neutral">Sign Up</Button>
+      </div>
+    </NavigationMenu>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +10,36 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        main: "var(--main)",
+        overlay: "var(--overlay)",
+        bg: "var(--bg)",
+        bw: "var(--bw)",
+        blank: "var(--blank)",
+        text: "var(--text)",
+        mtext: "var(--mtext)",
+        border: "var(--border)",
+        ring: "var(--ring)",
+        ringOffset: "var(--ring-offset)",
+
+        secondaryBlack: "#212121",
+      },
+      borderRadius: {
+        base: "0px",
+      },
+      boxShadow: {
+        shadow: "var(--shadow)",
+      },
+      translate: {
+        boxShadowX: "4px",
+        boxShadowY: "4px",
+        reverseBoxShadowX: "-4px",
+        reverseBoxShadowY: "-4px",
+      },
+      fontWeight: {
+        base: "500",
+        heading: "700",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
