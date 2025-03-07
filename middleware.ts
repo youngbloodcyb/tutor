4bba537a9817fe7ca84d089844af91c47ce7b240
Cameron Youngblood <cameron.youngblood@gmail.com>
2025-03-06 20:43:31 -0800

    hmm

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import { getSessionCookie } from "better-auth/cookies";
 
 export async function middleware(request: NextRequest) {
   const sessionCookie = getSessionCookie(request);
+  console.log(sessionCookie);
   if (!sessionCookie) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
