0092240894781eda9afd867cde60791cf450c377
Cameron Youngblood <cameron.youngblood@gmail.com>
2025-03-06 20:38:44 -0800

    fix?

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -2,6 +2,8 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/lib/db";
 import { user, session, account } from "@/lib/db/schema";
+import { nextCookies } from "better-auth/next-js";
+
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
@@ -14,4 +16,5 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  plugins: [nextCookies()],
 });
