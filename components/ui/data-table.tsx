1897e134c595ad017c72ea788daaa082630edc0e
Jared Young <79735672+jyoung10078@users.noreply.github.com>
2025-03-10 22:12:53 -0500

    Data Table Correct Values

--- components/ui/data-table.tsx
+++ components/ui/data-table.tsx
@@ -39,142 +39,75 @@ import {
 
 
 
-const data: Payment[] = [
+const data: Courses[] = [
   {
-    id: "m5gr84i9",
-    amount: 316,
-    status: "success",
-    email: "ken99@yahoo.com",
+    id: "graphing",
+    courseName: "Graphing",
+    type: "Interactive",
+    start: "Start",
+    status: "In progress",
+    length: 2,
   },
   {
-    id: "3u1reuv4",
-    amount: 242,
-    status: "success",
-    email: "Abe45@gmail.com",
+    id: "solving-equations",
+    courseName: "Solving Equations",
+    type: "Reading",
+    start: "Start",
+    status: "Not started",
+    length: 1,
   },
   {
-    id: "derv1ws0",
-    amount: 837,
-    status: "processing",
-    email: "Monserrat44@gmail.com",
-  },
-  {
-    id: "5kma53ae",
-    amount: 874,
-    status: "success",
-    email: "Silas22@gmail.com",
-  },
-  {
-    id: "bhqecj4p",
-    amount: 721,
-    status: "failed",
-    email: "carmella@hotmail.com",
+    id: "order-of-operations",
+    courseName: "Order of Operations",
+    type: "Interactive",
+    start: "Start",
+    status: "Not started",
+    length: 3,
   },
 ]
 
 
 
-export type Payment = {
-  id: string
-  amount: number
-  status: "pending" | "processing" | "success" | "failed"
-  email: string
+export type Courses = {
+    id: string,
+    courseName: string,
+    type: "Interactive" | "Reading",
+    start: "Start",
+    status: "In progress" | "Completed" | "Not started",
+    length: number,
 }
 
 
 
-export const columns: ColumnDef<Payment>[] = [
+export const columns: ColumnDef<Courses>[] = [
   {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
-    ),
+    accessorKey: "courseName",
+    header: "Course Name",
     cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
+      <div className="capitalize">{row.getValue("courseName")}</div>
     ),
-    enableSorting: false,
-    enableHiding: false,
+  },
+  {
+    accessorKey: "type",
+    header: "Type",
+    cell: ({ row }) => <div className="lowercase">{row.getValue("type")}</div>,
+  },
+  {
+    accessorKey: "start",
+    header: "Start",
+    cell: ({ row }) => <div className="lowercase">{row.getValue("start")}</div>,
   },
   {
     accessorKey: "status",
     header: "Status",
-    cell: ({ row }) => (
-      <div className="capitalize">{row.getValue("status")}</div>
-    ),
+    cell: ({ row }) => <div className="lowercase">{row.getValue("status")}</div>,
   },
   {
-    accessorKey: "email",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="noShadow"
-          size="sm"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Email
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
-      )
-    },
-    cell: ({ row }) => <div className="lowercase">{row.getValue("email")}</div>,
-  },
-  {
-    accessorKey: "amount",
-    header: () => <div className="text-right">Amount</div>,
-    cell: ({ row }) => {
-      const amount = parseFloat(row.getValue("amount"))
-
-      // Format the amount as a dollar amount
-      const formatted = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-      }).format(amount)
-
-      return <div className="text-right font-base">{formatted}</div>
-    },
-  },
-  {
-    id: "actions",
-    enableHiding: false,
-    cell: ({ row }) => {
-      const payment = row.original
-
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="noShadow" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(payment.id)}
-            >
-              Copy payment ID
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      )
-    },
+    accessorKey: "length",
+    header: "Length",
+    cell: ({ row }) => <div className="lowercase">{row.getValue("length")}</div>,
   },
 ]
-
 
 
 export default function DataTableDemo() {
