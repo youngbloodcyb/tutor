ce43492fe39839f4d3aca851a9dffa3d68f27b4e
Cameron Youngblood <cameron.youngblood@gmail.com>
2025-03-12 22:17:29 -0700

    [admin] add pages

---
+++ app/(admin)/admin/courses/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main>
+      <h1 className="text-2xl font-bold">Courses</h1>
+    </main>
+  );
+}
---
+++ app/(admin)/admin/users/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main>
+      <h1 className="text-2xl font-bold">Users</h1>
+    </main>
+  );
+}
