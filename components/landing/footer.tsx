e9085590eae4dbfdf4fa7a53547165d55b704677
Cameron Youngblood <cameron.youngblood@gmail.com>
2025-03-12 22:10:05 -0700

    update footer and header

--- components/landing/footer.tsx
+++ components/landing/footer.tsx
@@ -1,15 +1,22 @@
 export function Footer() {
   return (
     <footer className="m500:text-sm dark:bg-secondaryBlack z-30 bg-white px-5 py-5 text-center font-base">
-      Released under MIT License. The source code is available on{" "}
+      Built by{" "}
       <a
         target="_blank"
-        href="https://github.com/neobrutalism-templates/saas"
+        href="https://x.com/youngbloodcyb"
         className="font-heading underline"
       >
-        Github
+        Cameron Youngblood
       </a>
-      .
+      {" and "}
+      <a
+        target="_blank"
+        href="https://x.com/young_j9844"
+        className="font-heading underline"
+      >
+        Jared Young
+      </a>
     </footer>
   );
 }
