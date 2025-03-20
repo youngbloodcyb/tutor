98b363888df63567da151a93d077992d37f1fdc9
Jared Young <79735672+jyoung10078@users.noreply.github.com>
2025-03-19 23:37:17 -0500

    Got started on the settings and chat (loosely)

--- app/(app)/chats/page.tsx
+++ app/(app)/chats/page.tsx
@@ -1,6 +1,40 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { getSession } from "@/lib/auth/server";
+import { redirect } from "next/navigation";
 
 
-export default function Page() {
-    return (<h1>You found the chats page!</h1>)
+export default async function Page() {
+  const session = await getSession();
+
+  if (!session) {
+    redirect("/login");
+  }
+
+  return (
+    <main className="p-4 h-full">
+      <div className="grid grid-cols-4 gap-4 h-full">
+        <div className="col-span-1 h-full min-w-min">
+          <Card className="w-full h-full bg-bg">
+            <CardHeader className="leading-loose">
+            </CardHeader>
+          </Card>
+        </div>
+        <div className="col-span-3 flex flex-col gap-4">
+          <Card className="w-full h-full bg-bg">
+            <CardHeader>
+              <h1>You made it to the chats page!</h1>
+            </CardHeader>
+            <CardContent>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </main>
+  );
 }
-
--- app/(app)/settings/page.tsx
+++ app/(app)/settings/page.tsx
@@ -1,6 +1,47 @@
-
-
-export default function Page() {
-    return (<h1>You found the settings page!</h1>)
-}
-
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card";
+  import { getSession } from "@/lib/auth/server";
+  import { redirect } from "next/navigation";
+  
+  
+  export default async function Page() {
+    const session = await getSession();
+  
+    if (!session) {
+      redirect("/login");
+    }
+  
+    return (
+      <main className="p-4 h-full">
+        <div className="grid grid-cols-4 gap-4 h-full">
+          <div className="col-span-1 h-full min-w-min">
+            <Card className="w-full h-full bg-bg">
+              <CardHeader className="leading-loose">
+                <h3>General</h3>
+                <h3>Security</h3>
+                <h3>Integrations</h3>
+                <h3>Support</h3>
+                <h3>Organizations</h3>
+                <h3>Advanced</h3>
+              </CardHeader>
+            </Card>
+          </div>
+          <div className="col-span-3 flex flex-col gap-4">
+            <Card className="w-full h-full bg-bg">
+              <CardHeader>
+                <h1>You made it to the settings page!</h1>
+              </CardHeader>
+              <CardContent>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+      </main>
+    );
+  }
+  
