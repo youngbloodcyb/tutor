2db931321ced60e3b0813de6d98d1b2dd23b95f9
Cameron Youngblood <cameron.youngblood@gmail.com>
2025-03-30 21:07:21 -0700

    yolo swag money

--- app/(app)/courses/page.tsx
+++ app/(app)/courses/page.tsx
@@ -6,11 +6,20 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
-import DataTable from "@/app/(app)/courses/data-table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
-import { Plus, ArrowUpRight } from "lucide-react";
+import { Plus, ArrowUpRight, Edit, BookText } from "lucide-react";
 import { getSession } from "@/lib/auth/server";
 import { redirect } from "next/navigation";
+import { getAllCourses } from "@/lib/data/course";
+import Link from "next/link";
 
 const evaluations = [
   {
@@ -21,7 +30,7 @@ const evaluations = [
   {
     courseName: "Graphing",
     proficiency: 0.55,
-    style: "w-9 h-9 rounded-full bg-orange-300"
+    style: "w-9 h-9 rounded-full bg-orange-300",
   },
   {
     courseName: "Solving equations",
@@ -37,17 +46,48 @@ export default async function Page() {
     redirect("/login");
   }
 
+  const courses = await getAllCourses();
+
   return (
     <main className="p-4 h-full">
       <div className="grid grid-cols-4 gap-4 h-full">
         <div className="col-span-3 flex flex-col gap-4">
           <Card className="w-full h-full bg-bg">
             <CardHeader>
-              <CardTitle>Courses</CardTitle>
-              <CardDescription>All Courses</CardDescription>
+              <div className="flex justify-between items-center">
+                <div>
+                  <CardTitle>Courses</CardTitle>
+                  <CardDescription>All Courses</CardDescription>
+                </div>
+              </div>
             </CardHeader>
             <CardContent>
-              <DataTable />
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Name</TableHead>
+                    <TableHead>Status</TableHead>
+                    <TableHead></TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {courses.map((course) => (
+                    <TableRow key={course.id}>
+                      <TableCell>{course.name}</TableCell>
+                      <TableCell>In progress</TableCell>
+                      <TableCell>
+                        <div className="flex gap-2">
+                          <Link href={`/courses/${course.id}`}>
+                            <Button size="icon" variant="default">
+                              <BookText className="h-4 w-4" />
+                            </Button>
+                          </Link>
+                        </div>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
             </CardContent>
           </Card>
         </div>
