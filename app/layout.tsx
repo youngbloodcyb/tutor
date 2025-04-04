313e777dc7d6b5939f65ab759a63fba5dbd9cf4c
Cameron Youngblood <cameron.youngblood@gmail.com>
2025-04-04 16:35:02 -0700

    fix layout bug that sucked

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased bg-bg h-screen">
+      <body className="antialiased bg-bg  overflow-hidden">
         {children}
         <Toaster />
       </body>
