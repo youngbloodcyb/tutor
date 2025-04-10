4e53c9572f8ea725593cad12e9767fe96b5953f8
Cameron Youngblood <cameron.youngblood@gmail.com>
2025-04-09 23:12:56 -0700

    add 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+    <body>
+      <main className="p-20 flex justify-center items-center w-full">
+        <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] antialiased" />
+
+        <h1 className="text-4xl font-bold">Oops, page not found :(</h1>
+      </main>
+    </body>
+  );
+}
